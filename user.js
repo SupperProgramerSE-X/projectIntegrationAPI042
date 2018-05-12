9702b0e1d273e883f55ca68a83db567f9e5de776
thitima1509 <jillSE1509@hotmail.com>
2018-05-12 11:39:08 +0700

    user

---
+++ user.js
@@ -0,0 +1,33 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+$(function () {
+    
+    
+
+    $("#product").click(function() {
+        window.location.href = 'product.html';
+    });
+
+    $("#information").click(function() {
+        window.location.href = 'Payment.html';
+    });
+
+    $("#logistic").click(function() {
+        window.location.href = 'Logistic.html';
+    });
+
+      $("#logout").click(function() {
+        window.location.href = 'Login.html';
+    });
+});
